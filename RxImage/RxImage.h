a5ac0bf4100ac3ce5cd10bff5dc51d1e0117aef3
Hoang Anh Tuan <tuan.ha@teko.vn>
2021-05-26 21:54:59 +0700

    Initial Commit

---
+++ RxImage/RxImage.h
@@ -0,0 +1,18 @@
+//
+//  RxImage.h
+//  RxImage
+//
+//  Created by Digilife on 26/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxImage.
+FOUNDATION_EXPORT double RxImageVersionNumber;
+
+//! Project version string for RxImage.
+FOUNDATION_EXPORT const unsigned char RxImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxImage/PublicHeader.h>
+
+
