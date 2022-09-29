00bf0a3d40c18e568e7d757a94fb6155b09997a7
Hoang Anh Tuan <tuanhanh19981998@gmail.com>
2022-09-29 16:42:22 +0700

    Add some features

---
+++ RxImage/ProcessImage.swift
@@ -0,0 +1,22 @@
+//
+//  ProcessImage.swift
+//  RxImage
+//
+//  Created by Hoang Anh Tuan on 29/09/2022.
+//
+
+import Foundation
+
+public class ProcessImage {
+    public static func resizeImage() {
+        print("Start resize")
+    }
+    
+    static func compressImage() {
+        
+    }
+    
+    func cropImage() {
+        
+    }
+}
---
+++ RxImage/ProcessPixel.swift
@@ -0,0 +1,14 @@
+//
+//  ProcessPixel.swift
+//  RxImage
+//
+//  Created by Hoang Anh Tuan on 29/09/2022.
+//
+
+import Foundation
+
+class ProcessPixel {
+    func reducePixel() {
+        
+    }
+}
